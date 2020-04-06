daa7ffa3e2e48d3cfbe77c0a7ac0c78a6cf4eb3c
Philip (flip) Kromer <flip@mrflip.com>
2020-04-06 00:27:31 -0500

    feat: more, better data; fix: wrong key letter selection

--- data/parse_nytbee.js
+++ data/parse_nytbee.js
@@ -1,13 +1,14 @@
 #!/usr/bin/env node -r esm --harmony
-import fs       from 'fs'
-import JSSoup   from 'jssoup'
-import _        from 'lodash'
-import glob     from 'glob'
+// ^^^ Note that we are using modern JS features; npm install esm
+import _                /**/ from 'lodash'
+import fs                    from 'fs'
+import JSSoup                from 'jssoup'
+import glob                  from 'glob'
 //
-import Bee      from '../src/lib/Bee'
+import Bee                   from '../src/lib/Bee'
 
 // --- Setup
-// cd /data/ripd; wget -r -l100000 --no-clobber -nv https://nytbee.com/Bee_`date +"%Y%m%d"`.html && cd ~- && ./data/parse_nytbee.js
+// ( cd /data/ripd; wget -r -l100000 --no-clobber -nv https://nytbee.com/Bee_`date +"%Y%m%d"`.html && cd ~- && ./data/parse_nytbee.js )
 const data_dir = '/data/ripd/nytbee.com'
 
 // --- Parse
@@ -15,25 +16,53 @@ const AllWords = new Set()
 const AllObs   = new Set()
 const AllBees  = []
 
+const VOWELS   = new Set('a', 'e', 'i', 'o', 'u')
+
+// For all of the files we downloaded,
 glob(`${data_dir}/Bee_*.html`, (err, files) => {
   files.forEach((filename) => {
-    // console.log(filename)
-    const raw_html = fs.readFileSync(filename, 'utf8');
-    const doc = new JSSoup(raw_html);
-    const [wds_els, _skip, obs_els] = doc.findAll('div').filter((div) => div.attrs.class === 'answer-list')
+    console.log('Parsing', filename)
 
+    // Read in the file as text
+    const raw_html = fs.readFileSync(filename, 'utf8')
+    // Grab a JSSoup parser
+    const doc = new JSSoup(raw_html)
+
+    // Make the object we will populate
     const words = {}
 
+    // Find all the divs with class 'answer-list'
+    // We only want the first and the thirds, so destructure them out
+    const [wds_els, _skip, obs_els] = doc
+      .findAll('div')
+      .filter((div) => (
+        (div.attrs.class === 'answer-list')
+          || (div.attrs.id === 'answer-list')
+      ))
+
+    // Within the div for words,
+    words.wds     = wds_els
+      .text                             // Dump the HTML as text
+      .split(/\s+/)                     // Split on whitespace
+      .filter((ss) => (ss.length > 0))  // reject empty strings
+    // Within the div for rejected words,
+    if (obs_els) {
+      words.obs = obs_els               // go through its elements
+        .findAll('li')                  // to find all the <li> tags
+        .map((li) => li.text)           // And pull out the raw text from them
+    } else {
+      words.obs     = []
+    }
+
+    // Grab the date from the filename
     words.datestr = filename.replace(/.*Bee_/, '').replace(/\.html/, '')
 
-    words.wds     = wds_els.text.split(/\s+/).filter((ss) => (ss.length > 0))
-    words.obs     = obs_els.findAll('li').map((li) => li.text)
-    // words.datestr = datestr
+    // The page is now parsed; rest of this is turning it into the object we want
 
     // Find main letter
     const ltr_hist = {}
     words.wds.forEach(
-      (word) => word.split('').forEach(
+      (word) => _.uniq(word.split('')).forEach(
         (ltr) => (ltr_hist[ltr] = 1 + (ltr_hist[ltr]||0)))) // eslint-disable-line
 
     // add to lexicons
@@ -42,27 +71,42 @@ glob(`${data_dir}/Bee_*.html`, (err, files) => {
 
     words.letters = Object
       .entries(ltr_hist)
-      .sort(([_a, cta], [_b, ctb]) => ((cta > ctb) ? -1 : 1))
+      .sort(([wda, cta], [_b, ctb]) => {
+        if (cta > ctb)          return -1
+        if (cta < ctb)          return  1
+        if (VOWELS.has(wda))    return 1
+        return -1
+      })
       .map(([ll, _x]) => ll)
       .join('')
+    // console.log(words.letters)
+    // words.letters = words.letters
+
 
     words.letters = Bee.normalize(words.letters)
 
     AllBees.push(words)
   })
 
+  // Make the whole dictionary into the shapes we want
+
   const all_wds = Array.from(AllWords.values()).sort()
   const all_obs = Array.from(AllObs.values()).sort()
   // console.log(all_wds)
 
-  const all_ltrs = AllBees.map((bb)=> [bb.letters.toUpperCase(), bb.datestr])
-  all_ltrs.sort(([wa,da], [wb, db]) => (da < db ? 1 : -1))
+  const all_ltrs = AllBees.map((bb) => [bb.letters.toUpperCase(), bb.datestr])
+  all_ltrs.sort(([_wa, da], [_wb, db]) => (da < db ? 1 : -1))
 
-  fs.writeFileSync('./data/dict_nyt.json', JSON.stringify(all_wds), { encoding: 'utf8' });
-  fs.writeFileSync('./data/dict_obs.json', JSON.stringify(all_obs), { encoding: 'utf8' });
-  fs.writeFileSync('./data/bees.json',     JSON.stringify(all_ltrs), { encoding: 'utf8' });
+  // Write to disk
+
+  fs.writeFileSync('./data/dict_nyt.json', JSON.stringify(all_wds), { encoding: 'utf8' })
+  fs.writeFileSync('./data/dict_obs.json', JSON.stringify(all_obs), { encoding: 'utf8' })
+  fs.writeFileSync('./data/bees.json',     JSON.stringify(all_ltrs), { encoding: 'utf8' })
+
+  // Report on success
 
   console.log(all_ltrs.length)
+  console.log(all_wds.length)
 
   // import Dicts from './dicts'
   // console.log(Dicts)
